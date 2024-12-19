a7183a3e323a7a4df70e49c2ac3796f38c38cd81
rajgit5 <rajeshsahu0352@gmail.com>
2024-12-19 14:20:05 +0530

    home completed

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './NavBar'
+import Hero from './Hero';
+import Features from './Features';
+import Footer from './Footer';
+
+function Home() {
+  return (
+   <>
+    <Navbar/>
+    <Hero/>
+    <Features/>
+    <Footer/>
+   </>
+  )
+}
+
+export default Home
